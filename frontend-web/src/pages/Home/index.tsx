5080d8baebe2e29957b06e3ebf94032bf0565f4b
raphael-fleury <raphael.fleury9@gmail.com>
2020-09-18 16:04:30 -0300

    Creating home page

---
+++ frontend-web/src/pages/Home/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { ReactComponent as ArrowIcon } from '../../assets/arrow.svg';
+import { ReactComponent as GamerImage } from '../../assets/gamer.svg';
+import './styles.css'
+
+const Home = () => (
+    <div className="home-container">
+        <div className="home-text">
+            <h1 className="home-text-title">
+                Which games people like the most?
+            </h1>
+            <h3 className="home-text-subtitle">
+                Click in the button below and discover!
+            </h3>
+            <div className="home-actions">
+                <button className="home-btn">
+                    I WANT TO KNOW!
+                </button>
+                <div className="home-btn-icon">
+                    <ArrowIcon />
+                </div>
+            </div>
+        </div>
+        <GamerImage />
+    </div>
+);
+
+export default Home;
